ac64499a20905af674884737e5072780f4ca3cf1
DaniAkash <s.daniakash@gmail.com>
2020-06-12 16:28:51 +0530

    integrated react native

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-react-native-web`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
