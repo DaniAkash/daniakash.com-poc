127ba47853d30130d4a745e9af9272575e56d396
DaniAkash <s.daniakash@gmail.com>
2020-07-14 01:27:23 +0530

    add SEO for projects page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -4,12 +4,86 @@ import HomeLayout from "../components/Layouts/HomeLayout"
 import useColors from "../hooks/useColors"
 import { READING_FONT } from "../assets/styles/fonts"
 import { ResponsiveView } from "../LayoutEngine/PrimaryLayout"
+import { useStaticQuery, graphql } from "gatsby"
+import { BreadcrumbJsonLd, GatsbySeo } from "gatsby-plugin-next-seo"
 
 const Projects = () => {
+  const data = useStaticQuery(graphql`
+    query ProjectPageQuery {
+      site {
+        siteMetadata {
+          homepage
+          twitterHandle
+          title
+          description
+          copyright
+          menu {
+            label
+            path
+          }
+        }
+      }
+    }
+  `)
+
+  const {
+    title,
+    twitterHandle,
+    description,
+    homepage,
+    menu,
+  } = data.site.siteMetadata
+
   const colors = useColors()
+
+  const pageTitle = `Projects | ${title}`
 
   return (
     <HomeLayout>
+      <GatsbySeo
+        title={pageTitle}
+        description={description}
+        canonical={homepage}
+        twitter={{
+          handle: twitterHandle,
+          site: twitterHandle,
+          cardType: "summary_large_image",
+        }}
+        openGraph={{
+          url: homepage,
+          title: pageTitle,
+          description,
+          images: [
+            {
+              url: homepage + require("../assets/images/profile-pic.jpg"),
+              width: 460,
+              height: 460,
+              alt: "Dani Akash",
+            },
+            {
+              url: homepage + require("../assets/images/favicon.png"),
+              width: 600,
+              height: 600,
+              alt: "Dani Akash",
+            },
+          ],
+          site_name: pageTitle,
+        }}
+      />
+      <BreadcrumbJsonLd
+        itemListElements={[
+          {
+            position: 1,
+            name: menu[0].label,
+            item: homepage + menu[0].path,
+          },
+          {
+            position: 2,
+            name: menu[3].label,
+            item: homepage + menu[3].path,
+          },
+        ]}
+      />
       <ResponsiveView
         style={styles.projectContainer}
         mobileStyle={styles.mobileContainer}
